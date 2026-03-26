674e798fa02b6208348c6668202fc5165a607040
Andy Vladimir Javier Familia <andyjavierrd@gmail.com>
2026-03-26 13:41:53 -0400

    Modificar ProductDetailEvent.kt

--- app/src/main/java/edu/ucne/corebuild/presentation/detail/ProductDetailEvent.kt
+++ app/src/main/java/edu/ucne/corebuild/presentation/detail/ProductDetailEvent.kt
@@ -1,5 +1,8 @@
 package edu.ucne.corebuild.presentation.detail
 
+import edu.ucne.corebuild.domain.model.Component
+
 sealed interface ProductDetailEvent {
     data class LoadComponent(val id: Int) : ProductDetailEvent
+    data class AddToCart(val component: Component) : ProductDetailEvent
 }
