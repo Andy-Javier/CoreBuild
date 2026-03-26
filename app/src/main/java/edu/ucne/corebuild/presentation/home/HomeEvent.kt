059e83fc5ec5230c9609b109afa60fbf561481c3
Andy Vladimir Javier Familia <andyjavierrd@gmail.com>
2026-03-26 13:41:08 -0400

    Modificar HomeEvent.kt

--- app/src/main/java/edu/ucne/corebuild/presentation/home/HomeEvent.kt
+++ app/src/main/java/edu/ucne/corebuild/presentation/home/HomeEvent.kt
@@ -2,4 +2,5 @@ package edu.ucne.corebuild.presentation.home
 
 sealed interface HomeEvent {
     data object LoadComponents : HomeEvent
+    data class OnSearchQueryChange(val query: String) : HomeEvent
 }
