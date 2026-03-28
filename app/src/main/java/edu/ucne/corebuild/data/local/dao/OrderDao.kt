5ea3aa3ca244462b3fdee8f4c3bb2b49fd0d3e9b
Anderson Nunez Consoro <an8634335@gmail.com>
2026-03-27 21:10:11 -0400

    agregar OrderDao con operaciones CRUD.

---
+++ app/src/main/java/edu/ucne/corebuild/data/local/dao/OrderDao.kt
@@ -0,0 +1,17 @@
+package edu.ucne.corebuild.data.local.dao
+
+import androidx.room.*
+import edu.ucne.corebuild.data.local.entity.OrderEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface OrderDao {
+    @Query("SELECT * FROM orders ORDER BY date DESC")
+    fun getAllOrders(): Flow<List<OrderEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertOrder(order: OrderEntity)
+
+    @Delete
+    suspend fun deleteOrder(order: OrderEntity)
+}
