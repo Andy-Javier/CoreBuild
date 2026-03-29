71d20ca506483cb3a42374c9ec8f7640f3ab4025
Andy Vladimir Javier Familia <andyjavierrd@gmail.com>
2026-03-29 14:54:17 -0400

    modificar AuthUiState.kt para conteo de ordenes y total gastado

--- app/src/main/java/edu/ucne/corebuild/presentation/auth/AuthUiState.kt
+++ app/src/main/java/edu/ucne/corebuild/presentation/auth/AuthUiState.kt
@@ -6,6 +6,9 @@ data class AuthUiState(
     val user: User? = null,
     val isLogged: Boolean = false,
     val isLoading: Boolean = false,
+    val isCheckingSession: Boolean = true,
     val error: String? = null,
-    val successMessage: String? = null
+    val successMessage: String? = null,
+    val totalOrdersCount: Int = 0,
+    val totalSpent: Double = 0.0
 )
