a4571970b64a802336e371856d009aaa675ca748
Andy Vladimir Javier Familia <andyjavierrd@gmail.com>
2026-03-29 15:13:40 -0400

    agregar caregoria a HomeEvent.kt

--- app/src/main/java/edu/ucne/corebuild/presentation/home/HomeEvent.kt
+++ app/src/main/java/edu/ucne/corebuild/presentation/home/HomeEvent.kt
@@ -3,4 +3,8 @@ package edu.ucne.corebuild.presentation.home
 sealed interface HomeEvent {
     data object LoadComponents : HomeEvent
     data class OnSearchQueryChange(val query: String) : HomeEvent
+    data class OnCategoryChange(val category: String?) : HomeEvent
+    data object OnToggleBuildDialog : HomeEvent
+    data object OnAddFeaturedToCart : HomeEvent
+    data object ResetNavigation : HomeEvent
 }
