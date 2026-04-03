429473c026f8e4bb1ffa5500a4aeb873806b29fb
Anderson Nunez Consoro <an8634335@gmail.com>
2026-04-02 23:09:13 -0400

    implementar paginacion offline-first en ComponentRepositoryImp.

--- app/src/main/java/edu/ucne/corebuild/data/repository/ComponentRepositoryImpl.kt
+++ app/src/main/java/edu/ucne/corebuild/data/repository/ComponentRepositoryImpl.kt
@@ -1,8 +1,10 @@
 package edu.ucne.corebuild.data.repository
 
+import androidx.paging.*
 import edu.ucne.corebuild.data.local.dao.ComponentDao
+import edu.ucne.corebuild.data.local.database.CoreBuildDatabase
 import edu.ucne.corebuild.data.local.mapper.toDomain
-import edu.ucne.corebuild.data.remote.datasource.RemoteDataSource
+import edu.ucne.corebuild.data.paging.ComponentRemoteMediator
 import edu.ucne.corebuild.data.sync.SyncManager
 import edu.ucne.corebuild.domain.model.Component
 import edu.ucne.corebuild.domain.repository.ComponentRepository
@@ -14,6 +16,7 @@ import javax.inject.Inject
 
 class ComponentRepositoryImpl @Inject constructor(
     private val dao: ComponentDao,
+    private val db: CoreBuildDatabase,
     private val syncManager: SyncManager
 ) : ComponentRepository {
 
@@ -33,4 +36,17 @@ class ComponentRepositoryImpl @Inject constructor(
             .map { it?.toDomain() }
             .flowOn(Dispatchers.IO)
     }
+
+    @OptIn(ExperimentalPagingApi::class)
+    override fun getComponentsStream(): Flow<PagingData<Component>> =
+        Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                prefetchDistance = 5,
+                enablePlaceholders = false
+            ),
+            remoteMediator = ComponentRemoteMediator(dao, db, syncManager),
+            pagingSourceFactory = { dao.getComponentsPaged() }
+        ).flow
+         .map { pagingData -> pagingData.map { it.toDomain() } }
 }
