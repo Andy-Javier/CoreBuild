1c299a52622142dd1f6a186a1316bed682265643
Anderson Nunez Consoro <an8634335@gmail.com>
2026-04-02 23:08:44 -0400

    actualizar interfaz ComponentRepository con soporte de paginacion.

--- app/src/main/java/edu/ucne/corebuild/domain/repository/ComponentRepository.kt
+++ app/src/main/java/edu/ucne/corebuild/domain/repository/ComponentRepository.kt
@@ -1,9 +1,11 @@
 package edu.ucne.corebuild.domain.repository
 
+import androidx.paging.PagingData
 import edu.ucne.corebuild.domain.model.Component
 import kotlinx.coroutines.flow.Flow
 
 interface ComponentRepository {
     fun getComponents(): Flow<List<Component>>
     fun getComponentById(id: Int): Flow<Component?>
+    fun getComponentsStream(): Flow<PagingData<Component>>
 }
