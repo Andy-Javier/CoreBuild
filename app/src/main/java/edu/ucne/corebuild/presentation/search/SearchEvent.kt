b72ac674ff5365f12bb7c1b5badc5518d897c171
Andy <andyjavierrd@gmail.com>
2026-04-03 16:28:14 -0400

    Refactorizar a sealed interface

--- app/src/main/java/edu/ucne/corebuild/presentation/search/SearchEvent.kt
+++ app/src/main/java/edu/ucne/corebuild/presentation/search/SearchEvent.kt
@@ -1,9 +1,9 @@
 package edu.ucne.corebuild.presentation.search
 
-sealed class SearchEvent {
-    data class OnQueryChange(val query: String) : SearchEvent()
-    data class OnCategorySelect(val category: String) : SearchEvent()
-    data class OnBrandSelect(val brand: String) : SearchEvent()
-    data class OnPriceRangeChange(val min: Double?, val max: Double?) : SearchEvent()
-    object OnClearFilters : SearchEvent()
+sealed interface SearchEvent {
+    data class OnQueryChange(val query: String) : SearchEvent
+    data class OnCategorySelect(val category: String) : SearchEvent
+    data class OnBrandSelect(val brand: String) : SearchEvent
+    data class OnPriceRangeChange(val min: Double?, val max: Double?) : SearchEvent
+    data object OnClearFilters : SearchEvent
 }
