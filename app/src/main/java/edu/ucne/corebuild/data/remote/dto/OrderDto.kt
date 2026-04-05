f6f73dd12a3b49150863f0fc982f1f28e5e4e1dd
Andy Vladimir Javier Familia <andyjavierrd@gmail.com>
2026-04-05 15:21:14 -0400

    Eliminar comentarios que ya no son necesarios

--- app/src/main/java/edu/ucne/corebuild/data/remote/dto/OrderDto.kt
+++ app/src/main/java/edu/ucne/corebuild/data/remote/dto/OrderDto.kt
@@ -5,7 +5,7 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class OrderDto(
     val id: Int?,
-    val componentes: List<String>, // IDs o JSON de componentes
+    val componentes: List<String>,
     val precioTotal: Double,
     val fecha: Long,
     val estado: String
