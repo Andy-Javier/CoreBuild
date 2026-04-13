65591dc645613556052752e5b970fba7167913a3
Andy Vladimir Javier Familia <andyjavierrd@gmail.com>
2026-04-13 13:32:51 -0400

    Definición del DTO para la respuesta de subida de imágenes.

--- app/src/main/java/edu/ucne/corebuild/data/remote/dto/CloudinaryResponseDto.kt
+++ app/src/main/java/edu/ucne/corebuild/data/remote/dto/CloudinaryResponseDto.kt
@@ -1,9 +1,12 @@
 package edu.ucne.corebuild.data.remote.dto
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class CloudinaryResponseDto(
-    val secure_url: String? = null,
-    val public_id: String? = null
+    @Json(name = "secure_url")
+    val secureUrl: String? = null,
+    @Json(name = "public_id")
+    val publicId: String? = null
 )
