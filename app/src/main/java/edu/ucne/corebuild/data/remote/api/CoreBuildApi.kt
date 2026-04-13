278b7d9eb11adf26ecf26aed8276589d75ff3d51
Andy Vladimir Javier Familia <andyjavierrd@gmail.com>
2026-04-13 13:34:51 -0400

    Definir endpoints CRUD para administración de componentes.

--- app/src/main/java/edu/ucne/corebuild/data/remote/api/CoreBuildApi.kt
+++ app/src/main/java/edu/ucne/corebuild/data/remote/api/CoreBuildApi.kt
@@ -15,7 +15,7 @@ interface CoreBuildApi {
     suspend fun createCpu(@Body dto: CpuDto): Response<CpuDto>
 
     @PUT("Cpu/{id}")
-    suspend fun updateCpu(@Path("id") id: Int, @Body dto: CpuDto): Response<CpuDto>
+    suspend fun updateCpu(@Path("id") id: Int, @Body dto: CpuDto): Response<Unit>
 
     @DELETE("Cpu/{id}")
     suspend fun deleteCpu(@Path("id") id: Int): Response<Unit>
@@ -30,7 +30,7 @@ interface CoreBuildApi {
     suspend fun createGpu(@Body dto: GpuDto): Response<GpuDto>
 
     @PUT("Gpu/{id}")
-    suspend fun updateGpu(@Path("id") id: Int, @Body dto: GpuDto): Response<GpuDto>
+    suspend fun updateGpu(@Path("id") id: Int, @Body dto: GpuDto): Response<Unit>
 
     @DELETE("Gpu/{id}")
     suspend fun deleteGpu(@Path("id") id: Int): Response<Unit>
@@ -45,7 +45,7 @@ interface CoreBuildApi {
     suspend fun createMotherboard(@Body dto: MotherboardDto): Response<MotherboardDto>
 
     @PUT("Motherboard/{id}")
-    suspend fun updateMotherboard(@Path("id") id: Int, @Body dto: MotherboardDto): Response<MotherboardDto>
+    suspend fun updateMotherboard(@Path("id") id: Int, @Body dto: MotherboardDto): Response<Unit>
 
     @DELETE("Motherboard/{id}")
     suspend fun deleteMotherboard(@Path("id") id: Int): Response<Unit>
@@ -60,7 +60,7 @@ interface CoreBuildApi {
     suspend fun createRam(@Body dto: RamDto): Response<RamDto>
 
     @PUT("Ram/{id}")
-    suspend fun updateRam(@Path("id") id: Int, @Body dto: RamDto): Response<RamDto>
+    suspend fun updateRam(@Path("id") id: Int, @Body dto: RamDto): Response<Unit>
 
     @DELETE("Ram/{id}")
     suspend fun deleteRam(@Path("id") id: Int): Response<Unit>
@@ -75,7 +75,7 @@ interface CoreBuildApi {
     suspend fun createPsu(@Body dto: PsuDto): Response<PsuDto>
 
     @PUT("Psu/{id}")
-    suspend fun updatePsu(@Path("id") id: Int, @Body dto: PsuDto): Response<PsuDto>
+    suspend fun updatePsu(@Path("id") id: Int, @Body dto: PsuDto): Response<Unit>
 
     @DELETE("Psu/{id}")
     suspend fun deletePsu(@Path("id") id: Int): Response<Unit>
