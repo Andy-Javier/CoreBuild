59fe04d636cfde6ed0dba6c7933f603c5dd92c08
Andy Vladimir Javier Familia <andyjavierrd@gmail.com>
2026-04-13 13:32:37 -0400

    Configuración de credenciales para el servicio Cloudinary.

--- app/src/main/java/edu/ucne/corebuild/data/remote/image/CloudinaryConfig.kt
+++ app/src/main/java/edu/ucne/corebuild/data/remote/image/CloudinaryConfig.kt
@@ -2,6 +2,6 @@ package edu.ucne.corebuild.data.remote.image
 
 object CloudinaryConfig {
     const val CLOUD_NAME = "dsnaidobx"
-    const val UPLOAD_PRESET = "ml_default"
+    const val UPLOAD_PRESET = "corebuild_unsigned"
     const val UPLOAD_URL = "https://api.cloudinary.com/v1_1/$CLOUD_NAME/image/upload"
 }
